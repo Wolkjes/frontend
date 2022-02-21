4148a2892152a0e00ff4486a0f0bbb2aa06e3a0b
robbedelarbre <robbe.delarbre@gmail.com>
2022-02-21 15:31:08 +0100

    random

--- src/app/edit-co2-sensor/edit-co2-sensor.component.ts
+++ src/app/edit-co2-sensor/edit-co2-sensor.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { CampusComponent } from '../campus/campus.component';
 
 @Component({
   selector: 'app-edit-co2-sensor',
   templateUrl: './edit-co2-sensor.component.html',
-  styleUrls: ['./edit-co2-sensor.component.css']
+  styleUrls: ['./edit-co2-sensor.component.css'],
+  template: `<button (click)="CampusComponent.toggleShowEditSensor();
+             <com1 #closeEdit></com1>`
 })
 export class EditCo2SensorComponent implements OnInit {
 
@@ -11,5 +14,4 @@ export class EditCo2SensorComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
