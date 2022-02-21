d31dff89865888d3d0578a76dc89359fc99f84dc
robbejs <43341126+robbejs@users.noreply.github.com>
2022-02-21 15:31:33 +0100

    js

--- src/app/campus/campus.component.ts
+++ src/app/campus/campus.component.ts
@@ -10,11 +10,6 @@ export class CampusComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-      let node = document.createElement('script');
-      node.src = "../assets/js/campus.js";//Change to your js file
-      node.type = 'text/javascript';
-      node.async = true;
-      node.charset = 'utf-8';
-      document.getElementsByTagName('head')[0].appendChild(node);
+
     }
 }
