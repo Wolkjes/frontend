f5192654ce0ddaeca9fd55f702a8774c8e8adce8
robbedelarbre <robbe.delarbre@gmail.com>
2022-02-22 16:04:10 +0100

    made campus page more responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '450px',
+      },
+    },
   },
   plugins: [
     require('flowbite/plugin')
