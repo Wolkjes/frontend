0d99f99d06b4d1f5c0fafd8db694a1fd3eb6d258
robbedelarbre <robbe.delarbre@gmail.com>
2022-02-23 14:03:04 +0100

    Close button now works on edit-co2-sensor component

--- src/app/edit-co2-sensor/edit-co2-sensor.component.ts
+++ src/app/edit-co2-sensor/edit-co2-sensor.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CampusComponent } from '../campus/campus.component';
+import { EventEmitterService } from '../event-emitter.service';
 
 @Component({
   selector: 'app-edit-co2-sensor',
   templateUrl: './edit-co2-sensor.component.html',
   styleUrls: ['./edit-co2-sensor.component.css'],
-  template: `<button (click)="CampusComponent.toggleShowEditSensor();
-             <com1 #closeEdit></com1>`
 })
 export class EditCo2SensorComponent implements OnInit {
 
-  constructor() { }
+  constructor( private eventEmitterService: EventEmitterService) { }
 
   ngOnInit(): void {
+  }
+
+  close(){
+    this.eventEmitterService.close();
   }
 }
