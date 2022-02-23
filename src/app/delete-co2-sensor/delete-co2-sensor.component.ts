b893aeb45e99615594b1649c5e0b2b25caaaac92
robbedelarbre <robbe.delarbre@gmail.com>
2022-02-23 13:44:47 +0100

    close button in delete-co2-sensor-component works now

--- src/app/delete-co2-sensor/delete-co2-sensor.component.ts
+++ src/app/delete-co2-sensor/delete-co2-sensor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventEmitterService } from '../event-emitter.service';
 
 @Component({
   selector: 'app-delete-co2-sensor',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteCo2SensorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private eventEmitterService: EventEmitterService) { }
 
   ngOnInit(): void {
+  }
+
+  close() {
+    this.eventEmitterService.close();
   }
 
 }
