582352efbb3e875967710c847ddd60cc2dc6c5f1
Othello De Sutter <othello.desutter@uclllabs.be>
2022-02-28 15:05:34 +0100

    auth services accepts x-access-token

--- src/app/service/auth.interceptor.service.ts
+++ src/app/service/auth.interceptor.service.ts
@@ -3,15 +3,16 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { TokenStorageService } from './token-storage.service';
 import { Observable } from 'rxjs';
-const TOKEN_HEADER_KEY = 'Authorization';
+const TOKEN_HEADER_KEY = 'x-access-token';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private token: TokenStorageService) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authReq = req;
     const token = this.token.getToken();
+    console.log(token)
     if (token != null) {
-      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token) });
     }
     return next.handle(authReq);
   }
@@ -19,3 +20,4 @@ export class AuthInterceptor implements HttpInterceptor {
 export const authInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
 ];
+
