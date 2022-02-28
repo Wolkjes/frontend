391b34bf84c2a8269788f90b76906f61a617a3c1
Othello De Sutter <othello.desutter@uclllabs.be>
2022-02-28 15:07:18 +0100

    minor fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { TokenStorageService } from './service/token-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,19 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-
-  constructor(){}
+  
+  isLoggedIn = false;
+  constructor(private tokenStorageService: TokenStorageService) { }
 
   ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+    }
+  }
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.href = '/login';
   }
   title = 'wolkjes';
 }
